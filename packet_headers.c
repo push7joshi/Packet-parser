f202ee30d6aa9e2275478892f4daa1ab0630fd96
sridhar.ashk@gmail.com <srk@ubuntu.(none)>
2013-09-11 23:55:06 -0400

    ethernet parsing

--- packet_headers.c
+++ packet_headers.c
@@ -26,8 +26,11 @@
  *
  ***************************************/
 struct ethernet_h{
-    char srcAddress[6];
-    char destAddress[6];
+//	unsigned char preamble[7];
+//	unsigned char delimiter;
+
+    unsigned char destAddress[6];
+	unsigned char srcAddress[6];
     // if value < 1500(max allowed frame size); specifies length - ver802.2
     // else value > 1536; specifies which protocol is encapsulated in the payload - Ethernet II framing
     unsigned int etherType;
@@ -38,14 +41,13 @@ struct ip_h{
     unsigned char v_ihl; //internet header length
     unsigned char service; //Type of service - used to define the way routers handle the datagram
     unsigned short int total_len; //16 bits, max packet size - 2^16 - 65,536
-  
     unsigned short int identification; //Used along with src address to uniquely id a datagram
     unsigned int flags; // 00000xxx {Reserved = 0, Don't Fragment, Fragment}
     unsigned char ttl; //no. of hops
     unsigned char protocol; //http://bit.ly/c0xBMt list of ip protocols
     unsigned short checksum;
-    unsigned int32_t srcAddress;
-    unsigned int32_t destAddress;
+    unsigned int srcAddress;
+    unsigned int destAddress;
 };
 
 struct tcp_h{
@@ -80,11 +82,24 @@ int main(int argc, char * argv[]){
 
     printf("-------- Packet %d ------------\n",i);
     printf("Size: %d bytes\n",header.len);
-
+	//printf("Size %ld", sizeof(struct ethernet_h));
     /*ethernet header memory map*/
+	printf("src address- ");
     ethernet = (struct ethernet_h *) (packet);
+	unsigned char* src = ethernet->srcAddress;
+	int j;
+	for(j=0; j<6; j+=1){
+		printf("%02x:",(*(src+j)));
+	}
+	printf("\n");
 
-    //TODO: print src and dest MAC
+	printf("dest address- ");
+    	ethernet = (struct ethernet_h *) (packet);
+	unsigned char* dest = ethernet->destAddress;
+	for(j=0; j<6; j+=1){
+		printf("%02x:",(*(dest+j)));
+	}
+	printf("\n");
 
     /*cacluate start of IP header and map to struct*/
     ip = (struct ip_h *) (packet + sizeof(struct ethernet_h));
